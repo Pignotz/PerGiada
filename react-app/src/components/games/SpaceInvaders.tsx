03e60f611c5f488a54c357d1043f46c2f147111b
Pignotz <davide110494@gmail.com>
2025-02-25 22:08:58 +0100

    Creati cuori ridimensionabili con clip-path

--- react-app/src/components/games/SpaceInvaders.tsx
+++ react-app/src/components/games/SpaceInvaders.tsx
@@ -1,17 +1,18 @@
 import React, { useState, useEffect, useRef } from 'react';
 
+import './heart.css'; // Import the Heart.css file
 // Configurazione globale delle dimensioni e parametri di gioco
 const config = {
 	gameArea: {
-		width: () => 800,
-		height: () => 700,
-		backgroundColor: () => 'black',
+		width: (windowWidth: number) => windowWidth * 0.7,
+		    height: (windowHeight: number) => windowHeight * 0.3,
+		    backgroundColor: () => 'black',
 	},
 	player: {
-		width: () => config.gameArea.width() / 20,
-		height: () => config.player.width(),
-		speed: () => 10,
-		startX: () => config.gameArea.width() / 2 - config.player.width() / 2,
+		width: (gameWidth: number) => config.gameArea.width(gameWidth) / 20,
+		height: (gameWidth: number) => config.player.width(gameWidth),
+		speed: () => 20,
+		startX: (gameWidth: number) => config.gameArea.width(gameWidth) / 2 - config.player.width(gameWidth) / 2,
 		bottom: () => 30,
 		color: () => '#00ff00',
 		borderRadius: () => 5,
@@ -19,10 +20,10 @@ const config = {
 	invader: {
 		rowCount: () => 2,
 		colCount: () => 10,
-		width: () => (config.gameArea.width() / ((config.invader.colCount()*2) + 1)),
-		height: () => config.invader.width(),  // Manteniamo l'altezza uguale alla larghezza
-		xSpacing: () => config.invader.width(),  // Distanza orizzontale tra invaders
-		ySpacing: () => config.invader.width(),  // Distanza verticale tra invaders
+		width: (gameWidth: number) => (config.gameArea.width(gameWidth) / ((config.invader.colCount()*2) + 1)),
+		height: (gameWidth: number) => config.invader.width(gameWidth),  // Manteniamo l'altezza uguale alla larghezza
+		xSpacing: (gameWidth: number) => config.invader.width(gameWidth),  // Distanza orizzontale tra invaders
+		ySpacing: (gameWidth: number) => config.invader.width(gameWidth),  // Distanza verticale tra invaders
 		speed: () => 8,
 		color: () => 'red',
 	},
@@ -32,7 +33,7 @@ const config = {
 		speed: () => 5,
 		color: () => 'yellow',
 	},
-	gameOverLimit: () => 700,
+	gameOverLimit: (gameWidth: number) => config.gameArea.height(gameWidth) - 40 - config.player.height(gameWidth),
 };
 
 interface Bullet {
@@ -48,16 +49,27 @@ interface Invader {
 }
 
 const SpaceInvaders: React.FC = () => {
-	const [playerPos, setPlayerPos] = useState<number>(() => config.player.startX()); // Posizione iniziale del giocatore
+	const [windowSize, setWindowSize] = useState({ width: window.innerWidth, height: window.innerHeight });
+
+	const [playerPos, setPlayerPos] = useState<number>(() => config.player.startX(windowSize.width)); // Posizione iniziale del giocatore
 	const [bullets, setBullets] = useState<Bullet[]>([]); // Array di proiettili
 	const [invaders, setInvaders] = useState<Invader[]>([]); // Array di invaders
 	const [gameOver, setGameOver] = useState<boolean>(false); // Stato del gioco
 	const [score, setScore] = useState<number>(0); // Punteggio
 	const [canShoot, setCanShoot] = useState<boolean>(true); // Stato per il controllo dei tiri
 	const [paused, setPaused] = useState<boolean>(false); // Stato di pausa
-
 	const gameAreaRef = useRef<HTMLDivElement>(null);
 
+	useEffect(() => {
+	    const handleResize = () => {
+	      setWindowSize({ width: window.innerWidth, height: window.innerHeight });
+	    };
+
+	    window.addEventListener('resize', handleResize);
+	    return () => window.removeEventListener('resize', handleResize);
+	  }, []);
+
+	
 	useEffect(() => {
 	  if (gameAreaRef.current) {
 	    gameAreaRef.current.focus();
@@ -91,21 +103,25 @@ const SpaceInvaders: React.FC = () => {
 		for (let row = 0; row < config.invader.rowCount(); row++) {
 			for (let col = 0; col < config.invader.colCount(); col++) {
 				initialInvaders.push({
-					x: col * config.invader.xSpacing()*2 + config.invader.xSpacing(),
-					y: row * config.invader.ySpacing()*2 + config.invader.ySpacing(),
+					x: col * config.invader.xSpacing(windowSize.width)*2 + config.invader.xSpacing(windowSize.width),
+					y: row * config.invader.ySpacing(windowSize.width)*2 + config.invader.ySpacing(windowSize.width),
 					alive: true
 				});
 			}
 		}
 		setInvaders(initialInvaders);
-	}, []);
+	}, [windowSize]);
 
 	// Funzione per sparare un proiettile
 	const shootBullet = () => {
 		if (canShoot && !paused) {
 			setBullets(prevBullets => [
 				...prevBullets,
-				{ x: playerPos + 17, y: 600, hit: false }
+				{ 
+				                x: playerPos + config.player.width(windowSize.width) / 2 - config.bullet.width() / 2, 
+				                y: config.gameArea.height(windowSize.width) - config.player.bottom() - config.player.height(windowSize.width), 
+				                hit: false 
+				            }
 			]);
 			setCanShoot(false); // Disabilita il tiro per un breve periodo
 		}
@@ -122,7 +138,7 @@ const SpaceInvaders: React.FC = () => {
 						...bullet,
 						y: bullet.y - config.bullet.speed()
 					}))
-					.filter(bullet => bullet.y > 0 && !bullet.hit); // Rimuove i proiettili colpiti o fuori dallo schermo
+					.filter(bullet => !bullet.hit); // Filtra i proiettili colpiti
 
 				// Gestisce la collisione tra proiettili e invaders
 				setInvaders(prevInvaders => {
@@ -135,12 +151,12 @@ const SpaceInvaders: React.FC = () => {
 								if (
 									invader.alive &&
 									bullet.x > invader.x &&
-									bullet.x < invader.x + config.invader.width() &&
+									bullet.x < invader.x + config.invader.width(windowSize.width) &&
 									bullet.y > invader.y &&
-									bullet.y < invader.y + config.invader.height()
+									bullet.y < invader.y + config.invader.height(windowSize.width)
 								) {
 									updatedInvaders[i] = { ...invader, alive: false }; // L'invader è stato colpito
-									bullet.hit = true; // Segna il proiettile come usato
+									bullet.hit = true; // Segna il proiettile come colpito
 									setScore(prevScore => prevScore + 100); // Aggiungi punti
 									break; // Interrompe il ciclo per impedire ulteriori collisioni
 								}
@@ -151,12 +167,14 @@ const SpaceInvaders: React.FC = () => {
 					return updatedInvaders;
 				});
 
+				// Rimuove i proiettili colpiti
 				return newBullets;
 			});
 		}, 50);
 
 		return () => clearInterval(bulletMovementInterval);
 	}, [paused]);
+
 
 	// Gestisce il movimento degli invaders
 	useEffect(() => {
@@ -174,7 +192,7 @@ const SpaceInvaders: React.FC = () => {
 				return prevInvaders.map(invader => {
 					if (invader.alive) {
 						const newY = invader.y + config.invader.speed();
-						if (newY > config.gameOverLimit()) {
+						if (newY + config.invader.height(windowSize.width) > config.gameOverLimit(windowSize.width)) {
 							setGameOver(true); // Termina il gioco se un invader supera il limite
 						}
 						return { ...invader, y: newY };
@@ -193,7 +211,7 @@ const SpaceInvaders: React.FC = () => {
 
 		if (e.key === 'ArrowLeft' && playerPos > 0) {
 			setPlayerPos(prevPos => prevPos - config.player.speed());
-		} else if (e.key === 'ArrowRight' && playerPos < config.gameArea.width() - config.player.width()) {
+		} else if (e.key === 'ArrowRight' && playerPos < config.gameArea.width(windowSize.width) - config.player.width(windowSize.width)) {
 			setPlayerPos(prevPos => prevPos + config.player.speed());
 		} else if (e.key === ' ') {
 			shootBullet();
@@ -222,11 +240,17 @@ const SpaceInvaders: React.FC = () => {
 		}
 	}, [canShoot]);
 
+	
+	useEffect(() => {
+	    setPlayerPos(config.player.startX(windowSize.width));
+	}, [windowSize]);
+	
+	
 	// Funzioni per pausa e reset
 	const togglePause = () => setPaused(!paused);
 
 	const resetGame = () => {
-		setPlayerPos(config.player.startX());
+		setPlayerPos(config.player.startX(windowSize.width));
 		setBullets([]);
 		setScore(0);
 		setGameOver(false);
@@ -236,8 +260,8 @@ const SpaceInvaders: React.FC = () => {
 		for (let row = 0; row < config.invader.rowCount(); row++) {
 			for (let col = 0; col < config.invader.colCount(); col++) {
 				initialInvaders.push({
-					x: col * config.invader.xSpacing()*2 + config.invader.xSpacing(),
-					y: row * config.invader.ySpacing()*2 + config.invader.ySpacing(),
+					x: col * config.invader.xSpacing(windowSize.width)*2 + config.invader.xSpacing(windowSize.width),
+					y: row * config.invader.ySpacing(windowSize.width)*2 + config.invader.ySpacing(windowSize.width),
 					alive: true
 				});
 			}
@@ -276,24 +300,40 @@ const SpaceInvaders: React.FC = () => {
 			tabIndex={0} // Rende la div focalizzabile
 				style={{
 					position: 'relative',
-					width: `${config.gameArea.width()}px`,  // Funzione che restituisce la larghezza
-					height: `${config.gameArea.height()}px`,  // Funzione che restituisce l'altezza
+					width: `${config.gameArea.width(windowSize.width)}px`,  // Funzione che restituisce la larghezza
+					height: `${config.gameArea.height(windowSize.width)}px`,  // Funzione che restituisce l'altezza
 					backgroundColor: config.gameArea.backgroundColor(),
 					margin: '0 auto',
 					overflow: 'hidden',
 					outline: 'none', // Evita il bordo di focus visibile
 				}}
 			>
+			{/* Linea di game over */}
+			                <div style={{
+			                    position: 'absolute',
+			                    width: '100%',
+			                    height: '2px',
+			                    backgroundColor: 'red',
+			                    top: `${config.gameOverLimit(windowSize.width)}px`,
+			                }} />
 				{invaders.map((invader, index) => (
 					invader.alive &&
 					<div key={index}
 						className="invader" style={{
 							position: 'absolute',
-							width: `${config.invader.width()}px`,
-							height: `${config.invader.height()}px`,
+							width: `${config.invader.width(windowSize.width)}px`,
+							height: `${config.invader.height(windowSize.width)}px`,
 							backgroundColor: config.invader.color(),
 							left: invader.x,
 							top: invader.y,
+							// eslint-disable-next-line no-multi-str, no-template-curly-in-string
+							clipPath: `path("\
+							  M 0,${config.invader.width(windowSize.width) / 100 * 30}\
+							  A ${config.invader.width(windowSize.width) / 100 * 20},${config.invader.width(windowSize.width) / 100 * 20} 0,0,1 ${config.invader.width(windowSize.width) / 100 * 50},${config.invader.width(windowSize.width) / 100 * 30}\
+							  A ${config.invader.width(windowSize.width) / 100 * 20},${config.invader.width(windowSize.width) / 100 * 20} 0,0,1 ${config.invader.width(windowSize.width) / 100 * 100},${config.invader.width(windowSize.width) / 100 * 30}\
+							  Q ${config.invader.width(windowSize.width) / 100 * 100},${config.invader.width(windowSize.width) / 100 * 60} ${config.invader.width(windowSize.width) / 100 * 50},${config.invader.width(windowSize.width) / 100 * 100}\
+							  Q ${config.invader.width(windowSize.width) / 100 * 0},${config.invader.width(windowSize.width) / 100 * 60} 0,${config.invader.width(windowSize.width) / 100 * 30}\
+							  Z")`,
 						}}
 					/>
 				))}
@@ -303,8 +343,8 @@ const SpaceInvaders: React.FC = () => {
 						position: 'absolute',
 						left: `${playerPos}px`,
 						bottom: config.player.bottom(),
-						width: `${config.player.width()}px`,  // Invoca la funzione per ottenere la larghezza
-					    height: `${config.player.height()}px`,  // Invoca la funzione per ottenere l'altezza
+						width: `${config.player.width(windowSize.width)}px`,  // Invoca la funzione per ottenere la larghezza
+					    height: `${config.player.height(windowSize.width)}px`,  // Invoca la funzione per ottenere l'altezza
 					    backgroundColor: config.player.color(),
 						borderRadius: config.player.borderRadius(),
 					}}
